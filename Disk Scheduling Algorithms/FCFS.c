6e7fcfc571e1e047a7866d53a46fb50daa2d54ce
RangaSahithi <141542612+RangaSahithi@users.noreply.github.com>
2023-11-17 19:07:19 +0530

    Add files via upload

---
+++ Disk Scheduling Algorithms/FCFS.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+  int t[20], n, i, j, tohm[20], tot=0;
+  float avhm;
+  printf("enter the no.of tracks");
+  scanf("%d",&n);
+  printf("enter the tracks to be traversed");
+  for(i=2;i<n+2;i++)
+    scanf("%d",&t[i-1]);
+  for(i=1;i<n+1;i++)
+  {
+    tohm[i]=t[i+1]-t[i];
+    if(tohm[i]<0)
+      tohm[i]=tohm[i]*(-1);
+  }
+  for(i=1;i<n+1;i++)
+    tot+=tohm[i];
+  avhm=(float)tot/n;
+  printf("Tracks traversed\tDifference between tracks\n");
+  for(i=1;i<n+1;i++)
+    printf("%d\t\t\t%d\n",t[i-1],tohm[i]);
+  printf("\nAverage header movements:%f",avhm);
+}
